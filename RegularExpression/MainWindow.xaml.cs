b05d1ab157b8db200e127c26664913749e065968
Karcub <rokaszemumenyecske@gmail.com>
2021-06-06 11:45:33 +0200

    Added unit tests for input validation

---
+++ RegularExpression/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace RegularExpression
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private static string namePattern = @"^((\w{1,}-*\w{1,}\.*)\s{1}){1,2}\w{1,}-*\w{1,}$";
+        private static string phonePattern = @"^\((\d{3})\)[\s](\d{3})\-(\d{4})$";
+        private static string emailPattern = @"^\w{1,}\.*\w{1,}\@\w{1,}\.*\w{1,}\.[a-z]{2,3}$";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static bool ValidName(string nameString)
+        {
+            return Regex.IsMatch(nameString, namePattern);
+        }
+
+        public static bool ValidPhone(string phoneString)
+        {
+            return Regex.IsMatch(phoneString, phonePattern);
+        }
+
+        public static bool ValidEmail(string emailString)
+        {
+            return Regex.IsMatch(emailString, emailPattern);
+        }
+
+        public void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidName(NameString.Text))
+            {
+                MessageBox.Show("The name is invalid (only alphabetical characters are allowed)");
+            }
+            if (!ValidPhone(PhoneString.Text))
+            {
+                MessageBox.Show("The phone is invalid (correct pattern: (###) ###-####)");
+            }
+            if (!ValidEmail(EmailString.Text))
+            {
+                MessageBox.Show("The email is invalid (email should be: ex. mentor07@example.com)");
+            }
+            else if (ValidName(NameString.Text) && ValidPhone(PhoneString.Text) && ValidEmail(EmailString.Text))
+            {
+                MessageBox.Show("Congratulation, all fields match");
+            }
+        }
+
+        public void BtnReformat_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidPhone(PhoneInput.Text))
+            {
+                PhoneOutput.Text = "No reformatting needed";
+            }
+            else if (Regex.IsMatch(PhoneInput.Text, phonePattern))
+            {
+                PhoneOutput.Text = ReformatPhone(PhoneInput.Text);
+            }
+            else 
+            {
+                PhoneOutput.Text = "Reformatting is not possible";
+            }
+        }
+
+        public string ReformatPhone(string phoneString)
+        {
+
+            return Regex.Replace(phoneString, @"^\d{3}", @"");
+        }
+    }
+}
---
+++ RegularExpressionTests/Tests.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+
+namespace RegularExpressionTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void EmailEmptyString_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail(""));
+        }
+
+        [Test]
+        public void EmailWithMatchingPattern_Pass()
+        {
+            Assert.IsTrue(RegularExpression.MainWindow.ValidEmail("asd@asd.asd"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidEmail("asd_asd@asd.asd.asd"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidEmail("Asd@asd.asd"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidEmail("asd.asd@asd.asd"));
+        }
+
+        [Test]
+        public void EmailWithSpecialChars_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd:asd@asd.asd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd-asd@asd.asd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd?asd@asd.asd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd!asd@asd.asd"));
+        }
+
+        [Test]
+        public void EmailWithLongDomain_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd@asd.asdd"));
+            
+        }
+
+        [Test]
+        public void EmailWithoutAtSign_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd.asd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd.asddd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd.asd.asd"));
+        }
+
+        [Test]
+        public void EmailFirstOrLastCharSpace_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail(" asd@asd.asd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidEmail("asd@asd.asd "));
+        }
+        
+        [Test]
+        public void NameEmptyString_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName(""));
+        }
+
+        [Test]
+        public void NameWithOneChar_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("a"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName(" "));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("?"));
+        }
+
+        [Test]
+        public void NameWithMinTwoWordsAndTwoChars_pass()
+        {
+            Assert.IsTrue(RegularExpression.MainWindow.ValidName("as as"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidName("asd asd"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidName("as as as"));
+            Assert.IsTrue(RegularExpression.MainWindow.ValidName("a-s as as"));
+        }
+
+        [Test]
+        public void NameWithMoreThanThreeWords_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("as as as "));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("as as as -"));
+        }
+
+        [Test]
+        public void NameWithInvalidChars_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("as a-"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidName("as as-"));
+        }
+        
+        [Test]
+        public void PhoneEmptyString_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone(""));
+        }
+
+        [Test]
+        public void PhoneWithCorrectInput_Pass()
+        {
+            Assert.IsTrue(RegularExpression.MainWindow.ValidPhone("(123) 123-1234"));
+        }
+
+        [Test]
+        public void PhoneWithNonDigits_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("(asd) asd-asdd"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("(123) asd-1234"));
+        }
+
+        [Test]
+        public void PhoneWithMoreOrLessThenRequiredAmountOfChars_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("(123) 123-123444"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("(123) 123-1"));
+        }
+
+        [Test]
+        public void PhoneWithIncorrectPattern_Fail()
+        {
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("(123 1)23-1234"));
+            Assert.IsFalse(RegularExpression.MainWindow.ValidPhone("123) 1-231234("));
+        }
+    }
+}
